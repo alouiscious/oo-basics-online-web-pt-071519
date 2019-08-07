16d8be0163c3bfb53445cc52bbd120a44d6e71ab
Renee Sims <info@reneesoul.com>
2019-08-07 08:59:02 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,42 @@
+class Book
+  def initialize(title = "And Then There Were None")
+    @title = title
+    @author = author
+    # @genre = genre
+    # @page_count = page_count
+  end
+  
+  def title=(title)
+    @title = title
+  end
+  
+  def title
+  end
+  
+  def author=(author)
+    @author = author
+  end
+  
+  def author
+    @author
+  end
+  
+  def page_count=(num)  
+    @page_count = num
+  end
+  
+  def page_count
+    @page_count
+  end
+  def genre=(genre)
+    @genre = genre
+  end
+  
+  def genre
+    @genre
+  end
+  
+end
 
-
+# Book.new
+# Book.title
